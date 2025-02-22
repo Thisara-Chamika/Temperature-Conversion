16d154b40e879a8eba734a9e818fa8dd1205aed6
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-02-23 00:15:59 +0530

    update

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function convert(){
 if (toFahrenheit.checked) {
     temp = Number(textbox.value);
     temp = temp * 9 / 5 + 32 ;
-    result.textContent = temp.toFixed(1) + " F";
+    result.textContent = temp.toFixed(1) + " °F";
 } 
 else if (toCelsius.checked){
     temp = Number(textbox.value);
     temp = (temp-32) * (5/9) ;
-    result.textContent = temp.toFixed(1) + " C";
+    result.textContent = temp.toFixed(1) + " °C";
 }
 else{
     result.textContent = "Select a unit"
